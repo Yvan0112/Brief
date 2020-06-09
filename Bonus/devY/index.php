830dbdc1b3d5c86063fe08640b75f55ea3fdb50d
Yvan0112 <iryvan1@live.fr>
2020-06-09 17:28:18 +0200

    login,logout

--- Bonus/devY/index.php
+++ Bonus/devY/index.php
@@ -1,41 +1,22 @@
+<?php
+  // Initialiser la session
+  session_start();
+  // Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
+  if(!isset($_SESSION["username"])){
+    header("Location: login.php");
+    exit(); 
+  }
+?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>BRIEF</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
-    <header>
-
-        <h1> SIMPLON PREMIUM 2020</h1>
-        <img src="assets/img/simplon.png" alt="simplon.png">
-    </header>
-<main>
-<section class="S">
-    <div id="s1">
-        <h2>ACCEDEZ A VOS COMPETENCES</h2>
-        
+<html>
+  <head>
+  <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="sucess">
+    <h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
+    <p>C'est votre tableau de bord.</p>
+    <a href="logout.php">Déconnexion</a>
     </div>
-    <div id="s2">
-        <h2>CONNEXION</h2>
-        <form class="signin" method="POST" action="login.php">
-            <p>Adresse mail</p>
-            <input type="email" name="email" required placeholder="">
-            <p>Mot de passe</p>
-            <input type="password" name="password" required placeholder="">
-            <p>Mot de passe oublié?</p>
-            <button type="submit" name="submit">SE CONNECTER</button>
-            
-        </form>
-    </div>
-    </section>
-    </main>
-<footer>
-    <p>tous droits réservés simplon premium 2020</p>
-</footer>
-
-</body>
+  </body>
 </html>
--- Bonus/devY/login.php
+++ Bonus/devY/login.php
@@ -1,28 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
 <?php
-if (isset($_POST['submit']))
-{
-    $email = $_POST['email'];
-    $password = $_POST['email'];
-
-    $db = new PDO("mysql:host=localhost;dbname=login;charset=utf8", "root", "");
-        
-    $sql = "SELECT * FROM utilisateur where email = '$email'";
-    $result = $db ->prepare($sql);
-    $result->execute();
-    if($result->rowCount() > 0)
-    {
-        $data = $result->fetchAll();
-        if(password_verify($password,$data[0]["password"]))
-        {
-            echo "connexion établie";
-        }
-    }
-    else
-    {
-        $password = password_hash($password,PASSWORD_DEFAULT);
-        $sql = "INSERT INTO utilisateur (email,password) VALUES ('$email','$password')";
-        $req = $db->prepare($sql);
-        $req->execute();
-        echo"Enregistrement effectué";
-    }
+require('config.php');
+session_start();
+if (isset($_POST['username'])){
+  $username = stripslashes($_REQUEST['username']);
+  $username = mysqli_real_escape_string($conn, $username);
+  $password = stripslashes($_REQUEST['password']);
+  $password = mysqli_real_escape_string($conn, $password);
+    $query = "SELECT * FROM `utilisateur` WHERE username='$username' and password='".hash('sha256', $password)."'";
+  $result = mysqli_query($conn,$query) or die(mysql_error());
+  $rows = mysqli_num_rows($result);
+  if($rows==1){
+      $_SESSION['username'] = $username;
+      header("Location: index.php");
+  }else{
+    $message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
+  }
 }
+?>
+<form class="box" action="" method="post" name="login">
+<h1>SIMPLON PREMIUM 2020</h1>
+<h1 class="box-title">Connexion</h1>
+<input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur">
+<input type="password" class="box-input" name="password" placeholder="Mot de passe">
+<input type="submit" value="Connexion " name="submit" class="box-button">
+<p class="box-register">Vous êtes nouveau ici? <a href="register.php">S'inscrire</a></p>
+<?php if (! empty($message)) { ?>
+    <p class="errorMessage"><?php echo $message; ?></p>
+<?php } ?>
+</form>
+</body>
+</html>
